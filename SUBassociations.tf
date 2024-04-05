aca2d32ad6f303ac7862774a9e8087c9948264d2
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-05 16:46:23 +0200

    again

--- SUBassociations.tf
+++ SUBassociations.tf
@@ -1,4 +1,39 @@
-# resource "azurerm_management_group_subscription_association" "sub_audit_assoc" {
-#   management_group_id = data.azurerm_management_group.sub_audit.id
-#   subscription_id     = "/subscriptions/f34f6196-9ca8-445c-8e11-1db7ece881d4"
-# }
+resource "azurerm_management_group_subscription_association" "sub_audit_assoc" {
+  management_group_id = data.azurerm_management_group.mg_audit_id.id
+  subscription_id     = var.sub_audit_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_sandbox_assoc" {
+  management_group_id = data.azurerm_management_group.mg_sandbox_id.id
+  subscription_id     = var.sub_sandbox_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_alzcorp_assoc" {
+  management_group_id = data.azurerm_management_group.mg_corp_id.id
+  subscription_id     = var.sub_alzcorp_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_alzonline_assoc" {
+  management_group_id = data.azurerm_management_group.mg_online_id.id
+  subscription_id     = var.sub_alzonline_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_sharedservices_assoc" {
+  management_group_id = data.azurerm_management_group.mg_platform_id.id
+  subscription_id     = var.sub_sharedservices_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_identity_assoc" {
+  management_group_id = data.azurerm_management_group.mg_platform_id.id
+  subscription_id     = var.sub_identity_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_management_assoc" {
+  management_group_id = data.azurerm_management_group.mg_platform_id.id
+  subscription_id     = var.sub_management_id
+}
+
+resource "azurerm_management_group_subscription_association" "sub_connectivity_assoc" {
+  management_group_id = data.azurerm_management_group.mg_platform_id.id
+  subscription_id     = var.sub_connectivity_id
+}
--- data.tf
+++ data.tf
@@ -1,3 +1,51 @@
-# data "azurerm_management_group" "sub_audit" {
-#   name = var.audit_mg_name
-# }
+data "azurerm_management_group" "top_mg_id" {
+  name = var.top_mg_name
+}
+
+data "azurerm_management_group" "mg_platform_id" {
+  name = var.platform_mg_name
+}
+
+data "azurerm_management_group" "mg_lzs_id" {
+  name = var.lzs_mg_name
+}
+
+data "azurerm_management_group" "mg_decom_id" {
+  name = var.decom_mg_name
+}
+
+data "azurerm_management_group" "mg_sandbox_id" {
+  name = var.sandbox_mg_name
+}
+
+data "azurerm_management_group" "mg_audit_id" {
+  name = var.audit_mg_name
+}
+
+data "azurerm_management_group" "mg_sharedservices_id" {
+  name = var.sharedservices_mg_name
+}
+
+data "azurerm_management_group" "mg_identity_id" {
+  name = var.identity_mg_name
+}
+
+data "azurerm_management_group" "mg_management_id" {
+  name = var.management_mg_name
+}
+
+data "azurerm_management_group" "mg_connectivity_id" {
+  name = var.connectivity_mg_name
+}
+
+data "azurerm_management_group" "mg_corp_id" {
+  name = var.corp_mg_name
+}
+
+data "azurerm_management_group" "mg_online_id" {
+  name = var.online_mg_name
+}
+
+data "azurerm_management_group" "mg_hybrid_id" {
+  name = var.hybrid_mg_name
+}
