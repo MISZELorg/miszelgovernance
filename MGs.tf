b9a592b1c33bd40216d78e20aae60c59ba8c4d30
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-05 14:48:08 +0200

    add MGs

--- MGs.tf
+++ MGs.tf
@@ -1,11 +1,92 @@
-# # Create the parent management group
-# resource "azurerm_management_group" "mg_miszel" {
-#   display_name = "mg-miszel"
-# }
+# Create the top management group
+resource "azurerm_management_group" "mg_top" {
+  display_name = var.top_mg_name
+  name         = var.top_mg_name
+}
 
-# # Create the child management group
-# resource "azurerm_management_group" "mg_platform" {
-#   display_name               = "mg-platform"
-#   parent_management_group_id = azurerm_management_group.mg_miszel.id
-#   depends_on                 = [azurerm_management_group.mg_miszel]
-# }
+# Create the child management groups
+resource "azurerm_management_group" "mg_platform" {
+  display_name               = var.platform_mg_name
+  name                       = var.platform_mg_name
+  parent_management_group_id = azurerm_management_group.mg_top.id
+  depends_on                 = [azurerm_management_group.mg_top]
+}
+
+resource "azurerm_management_group" "mg_lzs" {
+  display_name               = var.lzs_mg_name
+  name                       = var.lzs_mg_name
+  parent_management_group_id = azurerm_management_group.mg_top.id
+  depends_on                 = [azurerm_management_group.mg_top]
+}
+
+resource "azurerm_management_group" "mg_decom" {
+  display_name               = var.decom_mg_name
+  name                       = var.decom_mg_name
+  parent_management_group_id = azurerm_management_group.mg_top.id
+  depends_on                 = [azurerm_management_group.mg_top]
+}
+
+resource "azurerm_management_group" "mg_sandbox" {
+  display_name               = var.sandbox_mg_name
+  name                       = var.sandbox_mg_name
+  parent_management_group_id = azurerm_management_group.mg_top.id
+  depends_on                 = [azurerm_management_group.mg_top]
+}
+
+resource "azurerm_management_group" "mg_audit" {
+  display_name               = var.audit_mg_name
+  name                       = var.audit_mg_name
+  parent_management_group_id = azurerm_management_group.mg_top.id
+  depends_on                 = [azurerm_management_group.mg_top]
+}
+
+# Create the grandchild management groups
+
+resource "azurerm_management_group" "mg_sharedservices" {
+  display_name               = var.sharedservices_mg_name
+  name                       = var.sharedservices_mg_name
+  parent_management_group_id = azurerm_management_group.mg_platform.id
+  depends_on                 = [azurerm_management_group.mg_platform]
+}
+
+resource "azurerm_management_group" "mg_identity" {
+  display_name               = var.identity_mg_name
+  name                       = var.identity_mg_name
+  parent_management_group_id = azurerm_management_group.mg_platform.id
+  depends_on                 = [azurerm_management_group.mg_platform]
+}
+
+resource "azurerm_management_group" "mg_management" {
+  display_name               = var.management_mg_name
+  name                       = var.management_mg_name
+  parent_management_group_id = azurerm_management_group.mg_platform.id
+  depends_on                 = [azurerm_management_group.mg_platform]
+}
+
+resource "azurerm_management_group" "mg_connectivity" {
+  display_name               = var.connectivity_mg_name
+  name                       = var.connectivity_mg_name
+  parent_management_group_id = azurerm_management_group.mg_platform.id
+  depends_on                 = [azurerm_management_group.mg_platform]
+}
+
+resource "azurerm_management_group" "mg_corp" {
+  display_name               = var.corp_mg_name
+  name                       = var.corp_mg_name
+  parent_management_group_id = azurerm_management_group.mg_lzs.id
+  depends_on                 = [azurerm_management_group.mg_lzs]
+}
+
+resource "azurerm_management_group" "mg_online" {
+  display_name               = var.online_mg_name
+  name                       = var.online_mg_name
+  parent_management_group_id = azurerm_management_group.mg_lzs.id
+  depends_on                 = [azurerm_management_group.mg_lzs]
+}
+
+resource "azurerm_management_group" "mg_hybrid" {
+  display_name               = var.hybrid_mg_name
+  name                       = var.hybrid_mg_name
+  parent_management_group_id = azurerm_management_group.mg_lzs.id
+  depends_on                 = [azurerm_management_group.mg_lzs]
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,17 @@
+# examples
 # variable "location" {}
 # variable "resource_group_name" {}
+
+variable "top_mg_name" {}
+variable "platform_mg_name" {}
+variable "lzs_mg_name" {}
+variable "decom_mg_name" {}
+variable "sandbox_mg_name" {}
+variable "audit_mg_name" {}
+variable "sharedservices_mg_name" {}
+variable "identity_mg_name" {}
+variable "management_mg_name" {}
+variable "connectivity_mg_name" {}
+variable "corp_mg_name" {}
+variable "online_mg_name" {}
+variable "hybrid_mg_name" {}
